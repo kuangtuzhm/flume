6198bc52ad9c2d664185d9ac51c3a445033b7b2a
Zhao Haiming <Zhao Haiming@123-PC>
2018-08-24 09:06:28 +0800

    1111

---
+++ flume-logBase/src/main/java/com/zealot/flume/log/bean/LogInfo.java
@@ -0,0 +1,36 @@
+/*
+ * COPYRIGHT. ShenZhen JiMi Technology Co., Ltd. 2018.
+ * ALL RIGHTS RESERVED.
+ *
+ * No part of this publication may be reproduced, stored in a retrieval system, or transmitted,
+ * on any form or by any means, electronic, mechanical, photocopying, recording, 
+ * or otherwise, without the prior written permission of ShenZhen JiMi Network Technology Co., Ltd.
+ *
+ * Amendment History:
+ * 
+ * Date                   By              Description
+ * -------------------    -----------     -------------------------------------------
+ * 2018年8月24日    Zhao Haiming         Create the class
+ * http://www.jimilab.com/
+*/
+
+package com.zealot.flume.log.bean;
+
+/**
+ * @FileName LogInfo.java
+ * @Description: 
+ *
+ * @Date 2018年8月24日 上午9:03:19
+ * @author Zhao Haiming
+ * @version 1.0
+ */
+public class LogInfo {
+
+    private String uuid;
+    
+    private String projectId;
+    
+    private String serverIp;
+    
+    
+}
